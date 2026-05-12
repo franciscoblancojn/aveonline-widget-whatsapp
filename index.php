2db5f89fe342f931d0ef3e427254e961b97314cc
Francisco Blanco <blancofrancisco34@gmail.com>
2026-05-12 12:56:32 -0500

    change update

--- index.php
+++ index.php
@@ -24,54 +24,12 @@ define("AVWW_URL",plugin_dir_url(__FILE__));
 define("AVWW_BASENAME",plugin_basename(__FILE__));
 
 require_once AVWW_DIR . 'update.php';
-github_updater_plugin_wordpress([
+github_updater_plugin_wordpress_function([
     'basename'=>AVWW_BASENAME,
     'dir'=>AVWW_DIR,
     'file'=>"index.php",
     'path_repository'=>'franciscoblancojn/aveonline-widget-whatsapp',
     'branch'=>'master',
-    'token_array_split'=>[
-        "g",
-        "h",
-        "p",
-        "_",
-        "G",
-        "4",
-        "W",
-        "E",
-        "W",
-        "F",
-        "p",
-        "V",
-        "U",
-        "E",
-        "F",
-        "V",
-        "x",
-        "F",
-        "U",
-        "n",
-        "b",
-        "M",
-        "k",
-        "P",
-        "R",
-        "x",
-        "o",
-        "f",
-        "t",
-        "Y",
-        "8",
-        "z",
-        "j",
-        "t",
-        "4",
-        "E",
-        "x",
-        "b",
-        "i",
-        "9"
-    ]
 ]);
 
 require_once AVWW_DIR . 'src/api/_.php';
